83d9cc7c4dbcf38a7ff9ce3eb82876826789afbe
jingshuai <jin.gshuai@qq.com>
2018-10-21 01:03:12 +0800

    测试concurrentHashMap和"\\|"

--- src/test/java/com/jing/java_example/ConCurrentHashMapExam.java
+++ src/test/java/com/jing/java_example/ConCurrentHashMapExam.java
@@ -6,7 +6,10 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class ConCurrentHashMapExam extends  JavaExampleApplicationTests{
 
-
+    /**
+     * 测试当concurrentHashMap没有key，然后用remove会不会报错
+     * 结果：不会报错
+     */
     private ConcurrentHashMap<String, String> concurrentHashMap
             = new ConcurrentHashMap<>();
 
--- src/test/java/com/jing/java_example/TestLine.java
+++ src/test/java/com/jing/java_example/TestLine.java
@@ -6,11 +6,16 @@ import java.util.Arrays;
 
 public class TestLine {
 
-    String LINE = ";";
+    /**
+     * 测试组合字符串加入"|"，是否需要加入转义字符"\\|"。
+     * 结果，不需要。而且这样通过str.split("\\|")错误。解析成   a a \ b b
+     * 但是解析时需要加入转义字符"\\|",不然解析的结果和split("")是一样的
+     */
+    String LINE = "\\|";
     @Test
     public void testLine(){
         String str = "aa"+LINE +"bb";
-        String[] arr = str.split("");
+        String[] arr = str.split(LINE);
         System.out.println(Arrays.toString(arr));
     }
 }
